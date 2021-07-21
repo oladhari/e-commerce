691d565a7df8fe900ecb6768eeb5aed291ec9dfa
oladhari <oussama.ladhari@gmail.com>
2021-07-21 21:10:26 +0900

    fix: serve media and static files from local server django

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("api-auth/", include("rest_framework.urls")),
@@ -8,4 +10,4 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
     path("admin/", admin.site.urls),
     path("api/", include("market.api.urls")),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
