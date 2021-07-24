4f565fca3b2637cf12dd3ea6c1d9f14a1e696133
farid212 <farid.hammouten@gmail.com>
2021-07-25 00:25:09 +0900

    have to make it better

---
+++ src/components/Category.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Grid } from 'semantic-ui-react'
+
+const Category = ({ cat, key }) => (
+    <Grid.Row key={key}>
+        <Grid.Column>{cat}</Grid.Column>
+    </Grid.Row>
+)
+
+export default Category
