778c75be3aa55845028f8b0e393b0a1b8fd676fa
farid212 <farid.hammouten@gmail.com>
2021-07-30 04:38:05 +0900

    basicsCI

---
+++ src/components/CartItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const CartItem = () => (
+  <div style={{ height: "50px", border: "1px red solid", padding: "10px" }}>
+    this is a cart element
+  </div>
+);
+
+export default CartItem;
