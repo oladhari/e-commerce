9314c6d5cfbd5dd69fc1c8c871a32fe4efdd53b3
farid212 <farid.hammouten@gmail.com>
2021-07-30 04:40:41 +0900

    update routes

--- src/constants/indexRoutes.js
+++ src/constants/indexRoutes.js
@@ -1,10 +1,12 @@
 import ProductList from "../views/ProductList";
-import CategoriesList from "../views/CategoriesList";
+import CategoriesList from "../views/CategoriesLayout";
 import Login from "../views/Login";
 import Signup from "../views/Signup";
 import HomepageLayout from "../views/Home";
+import Cart from "../views/Cart";
 
 const indexRoutes = [
+  { path: "/cart", component: Cart },
   { path: "/categories", component: CategoriesList },
   { path: "/products", component: ProductList },
   { path: "/login", component: Login },
