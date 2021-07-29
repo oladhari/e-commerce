0b05e84643168ed297a1c7b898f36a5d99221282
farid212 <farid.hammouten@gmail.com>
2021-07-30 04:40:25 +0900

    cart basic view

---
+++ src/views/Cart.js
@@ -0,0 +1,11 @@
+import React from "react";
+import CardItem from "../components/CartItem";
+
+const Cart = () => (
+  <>
+    <div>this is the cart page</div>
+    <CardItem />
+  </>
+);
+
+export default Cart;
