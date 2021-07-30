1cb1210b9cc9eaf5143de4bb4e0952bae012fa63
farid212 <farid.hammouten@gmail.com>
2021-07-31 07:37:55 +0900

    modify banner add more content

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,28 @@
 import React from "react";
+import { Segment, Grid } from "semantic-ui-react";
 
-const Banner = ({ data }) => <h1 style={{ fontSize: "400%" }}>{data}</h1>;
+const Banner = ({ title, bgImageLink, txtColor }) => (
+  <Segment
+    style={{ padding: "8em 0em" }}
+    vertical
+    style={{
+      backgroundImage: `url(${bgImageLink})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      height: "300px",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    }}
+  >
+    <Grid container>
+      <Grid.Row>
+        <Grid.Column textAlign="center">
+          <h1 style={{ fontSize: "400%", color: `${txtColor}` }}>{title}</h1>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  </Segment>
+);
 
 export default Banner;
