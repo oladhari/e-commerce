a0bccc76b52e35142a1e2d5767eb4eff50f80ee0
farid212 <farid.hammouten@gmail.com>
2021-07-31 07:41:58 +0900

    update Layout but Helmet net accepted yet have to debug

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,125 +1,31 @@
-import {
-  Container,
-  Divider,
-  Dropdown,
-  Grid,
-  Header,
-  Image,
-  List,
-  Menu,
-  Segment,
-} from "semantic-ui-react";
-import { Link, withRouter } from "react-router-dom";
-
 import React from "react";
+import { withRouter } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 import { connect } from "react-redux";
 import { logout } from "../store/actions/auth";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
 
-class CustomLayout extends React.Component {
-  render() {
-    const { authenticated } = this.props;
-    return (
-      <div>
-        <Menu inverted>
-          <Container>
-            {authenticated ? (
-              <>
-                <Menu.Item header onClick={() => this.props.logout()}>
-                  Logout
-                </Menu.Item>
-                <Link to="/">
-                  <Menu.Item header>Home</Menu.Item>
-                </Link>
-              </>
-            ) : (
-              <React.Fragment>
-                <Link to="/">
-                  <Menu.Item header>Home</Menu.Item>
-                </Link>
-                <Link to="/login">
-                  <Menu.Item header>Login</Menu.Item>
-                </Link>
-                <Link to="/signup">
-                  <Menu.Item header>Signup</Menu.Item>
-                </Link>
-              </React.Fragment>
-            )}
-            <Link to="/products">
-              <Menu.Item header>Products</Menu.Item>
-            </Link>
-            <Link to="/categories">
-              <Menu.Item header>Categories</Menu.Item>
-            </Link>
-          </Container>
-        </Menu>
+const Layout = ({ authenticated, title, description, children }) => {
+  return (
+    <div>
+      {/* <Helmet>
+        <title>
+          {title ? title + " - E-Commerce Boilerplate" : "React.js Boilerplate"}
+        </title>
+        <meta
+          name="description"
+          content={description || "React.js Boilerplate"}
+        />
+      </Helmet> */}
+      <Navbar authenticated={authenticated} />
 
-        {this.props.children}
+      {children}
 
-        <Segment
-          inverted
-          vertical
-          style={{ margin: "5em 0em 0em", padding: "5em 0em" }}
-        >
-          <Container textAlign="center">
-            <Grid divided inverted stackable>
-              <Grid.Column width={3}>
-                <Header inverted as="h4" content="Group 1" />
-                <List link inverted>
-                  <List.Item as="a">Link One</List.Item>
-                  <List.Item as="a">Link Two</List.Item>
-                  <List.Item as="a">Link Three</List.Item>
-                  <List.Item as="a">Link Four</List.Item>
-                </List>
-              </Grid.Column>
-              <Grid.Column width={3}>
-                <Header inverted as="h4" content="Group 2" />
-                <List link inverted>
-                  <List.Item as="a">Link One</List.Item>
-                  <List.Item as="a">Link Two</List.Item>
-                  <List.Item as="a">Link Three</List.Item>
-                  <List.Item as="a">Link Four</List.Item>
-                </List>
-              </Grid.Column>
-              <Grid.Column width={3}>
-                <Header inverted as="h4" content="Group 3" />
-                <List link inverted>
-                  <List.Item as="a">Link One</List.Item>
-                  <List.Item as="a">Link Two</List.Item>
-                  <List.Item as="a">Link Three</List.Item>
-                  <List.Item as="a">Link Four</List.Item>
-                </List>
-              </Grid.Column>
-              <Grid.Column width={7}>
-                <Header inverted as="h4" content="Footer Header" />
-                <p>
-                  Extra space for a call to action inside the footer that could
-                  help re-engage users.
-                </p>
-              </Grid.Column>
-            </Grid>
-
-            <Divider inverted section />
-            <Image centered size="mini" src="/logo.png" />
-            <List horizontal inverted divided link size="small">
-              <List.Item as="a" href="#">
-                Site Map
-              </List.Item>
-              <List.Item as="a" href="#">
-                Contact Us
-              </List.Item>
-              <List.Item as="a" href="#">
-                Terms and Conditions
-              </List.Item>
-              <List.Item as="a" href="#">
-                Privacy Policy
-              </List.Item>
-            </List>
-          </Container>
-        </Segment>
-      </div>
-    );
-  }
-}
+      <Footer />
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
@@ -133,6 +39,4 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-export default withRouter(
-  connect(mapStateToProps, mapDispatchToProps)(CustomLayout)
-);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Layout));
