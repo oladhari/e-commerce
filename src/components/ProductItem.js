a8bc745b347a5c4e06404a228e62dc71a81da359
farid212 <farid.hammouten@gmail.com>
2021-08-04 22:30:07 +0900

    productItem

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CartContext } from "../contexts/CartContext";
+import { formatNumber } from "../helpers/utils";
 
 import { Icon, Card, Button } from "semantic-ui-react";
 
-const Product = ({ data, key }) => {
+const Product = ({ product, key }) => {
+  const { addProduct, cartItems, increase } = useContext(CartContext);
+
+  const isInCart = ({ product }) => {
+    return !!cartItems.find((item) => item.id === product.id);
+  };
+
   return (
     <Card key={key} style={{ padding: "1rem" }}>
       <div
         style={{
           height: "200px",
-          backgroundImage: `url(${data.image})`,
+          backgroundImage: `url(${product.image})`,
           backgroundSize: "contain",
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
         }}
       ></div>
       <Card.Content>
-        <Card.Meta>{data.title}</Card.Meta>
+        <Card.Meta>{product.title}</Card.Meta>
         <Card.Header style={{ padding: "10px 0", fontSize: "200%" }}>
-          {data.price} ${" "}
+          {product.price} ${" "}
         </Card.Header>
-        <Card.Description>{data.description}</Card.Description>
+        <Card.Description>{product.description}</Card.Description>
       </Card.Content>
       <Card.Content
         extra
@@ -31,12 +39,30 @@ const Product = ({ data, key }) => {
             <Icon name="arrow right" />
           </Button.Content>
         </Button>
-        <Button animated="vertical" style={{ width: "100px" }}>
-          <Button.Content hidden>Add to cart</Button.Content>
-          <Button.Content visible>
-            <Icon name="shop" />
-          </Button.Content>
-        </Button>
+        {isInCart(product) && (
+          <Button
+            animated="vertical"
+            style={{ width: "100px" }}
+            onClick={() => increase(product)}
+          >
+            <Button.Content hidden>Add more</Button.Content>
+            <Button.Content visible>
+              <Icon name="plus" />
+            </Button.Content>
+          </Button>
+        )}
+        {!isInCart(product) && (
+          <Button
+            animated="vertical"
+            style={{ width: "100px" }}
+            onClick={() => addProduct(product)}
+          >
+            <Button.Content hidden>Add to cart</Button.Content>
+            <Button.Content visible>
+              <Icon name="shop" />
+            </Button.Content>
+          </Button>
+        )}
       </Card.Content>
     </Card>
   );
