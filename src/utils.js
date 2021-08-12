8516305bd3e728ce2f8cf63fc4afe75cb6962e4c
oladhari <oussama.ladhari@gmail.com>
2021-08-12 09:26:45 +0900

    fix: delete header cors

--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,6 @@ import { endpoint } from "./constants";
 export const authAxios = Axios.create({
   baseURL: endpoint,
   headers: {
-    "Access-Control-Allow-Origin": "*",
     Authorization: `Token ${localStorage.getItem("token")}`,
   },
 });
